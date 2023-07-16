bed38db8c8963d5966da8e66ce7a9d41c6d6d0e2
callmeeve <wididiky38@gmail.com>
2023-07-16 16:45:46 +0700

    upload

--- components/UploadFile.jsx
+++ components/UploadFile.jsx
@@ -1,7 +1,19 @@
-import { useState } from 'react';
 import React from 'react';
+import { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+  Input
+} from "@material-tailwind/react";
 
-export default function Upload() {
+export default function Modals() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
   const [file, setFile] = useState(null);
   const [userId, setUserId] = useState('');
 
@@ -39,25 +51,108 @@ export default function Upload() {
   };
 
   return (
-    <div>
-      <h1>Upload File</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="file">File:</label>
-          <input type="file" id="file" onChange={handleFileChange} />
-        </div>
-        <div>
-          <label htmlFor="userId">User ID:</label>
-          <input
-            type="text"
-            id="userId"
-            value={userId}
-            onChange={handleUserIdChange}
-          />
-        </div>
-        <button type="submit">Upload</button>
-      </form>
-    </div>
+    <>
+      <Button onClick={handleOpen} variant="gradient">
+        Tambah Data
+      </Button>
+      <Dialog size="md" open={open} handler={handleOpen}>
+        <DialogHeader>Tambah Folder</DialogHeader>
+        <DialogBody divider>
+          <form onSubmit={handleSubmit}>
+            <Input size="md" label="Nama Folder" />
+            <input
+              type="file" id="file" onChange={handleFileChange}
+              class="cursor-pointer my-3 w-full text-sm text-slate-500 file:cursor-pointer file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-blue-400 file:text-white hover:file:bg-violet-100"
+            />
+            <Input
+              size="md"
+              type="text"
+              id="userId"
+              label="User ID"
+              value={userId}
+              onChange={handleUserIdChange}
+            />
+            <div className="flex flex-row items-center justify-end my-2">
+              <Button
+                variant="text"
+                color="red"
+                onClick={handleOpen}
+                className="mr-1"
+              >
+                <span>Batal</span>
+              </Button>
+              <Button variant="gradient" type='submit' color="green" onClick={handleOpen}>
+                <span>Submit</span>
+              </Button>
+            </div>
+          </form>
+        </DialogBody>
+        <DialogFooter/>
+      </Dialog>
+    </>
   );
 }
+
+// import { useState } from 'react';
+
+
+// export default function Upload() {
+//   const [file, setFile] = useState(null);
+//   const [userId, setUserId] = useState('');
+
+//   const handleFileChange = (event) => {
+//     setFile(event.target.files[0]);
+//   };
+
+//   const handleUserIdChange = (event) => {
+//     setUserId(event.target.value);
+//   };
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+
+//     try {
+//       const formData = new FormData();
+//       formData.append('file', file);
+//       formData.append('userId', userId);
+
+//       const response = await fetch('/api/file/create', {
+//         method: 'POST',
+//         body: formData,
+//       });
+
+//       if (response.ok) {
+//         const data = await response.json();
+//         console.log('File uploaded successfully:', data);
+//       } else {
+//         const error = await response.json();
+//         console.error('File upload failed:', error);
+//       }
+//     } catch (error) {
+//       console.error('File upload failed:', error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h1>Upload File</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label htmlFor="file">File:</label>
+//           <input type="file" id="file" onChange={handleFileChange} />
+//         </div>
+//         <div>
+//           <label htmlFor="userId">User ID:</label>
+//           <input
+//             type="text"
+//             id="userId"
+//             value={userId}
+//             onChange={handleUserIdChange}
+//           />
+//         </div>
+//         <button type="submit">Upload</button>
+//       </form>
+//     </div>
+//   );
+// }
 
